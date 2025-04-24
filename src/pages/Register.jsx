2f3e32c1e80243d6fd63b6b9e04fb1ee47d55cb6
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-04-24 15:32:19 +0530

    [Rakshit] Add: Register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthTemplate from '../components/common/AuthTemplate'
+
+const Register = () => {
+  return (
+    <div>
+      <AuthTemplate type="register"/>
+    </div>
+  )
+}
+
+export default Register
